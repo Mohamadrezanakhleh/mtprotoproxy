529580a3db00513604883c95e29d24873ecedf0d
Mohamadrezanakhleh <44848120+Mohamadrezanakhleh@users.noreply.github.com>
2018-11-26 14:01:08 +0330

    Add files via upload
    Added server time correction  option  to config file

---
+++ telegram-cloud-document-4-6044010796499862709.py
@@ -0,0 +1,15 @@
+PORT = 3256
+
+# name -> secret (32 hex chars)
+USERS = {
+    "tg":  "00000000000000000000000000000000",
+    "tg2": "0123456789abcdef0123456789abcdef"
+}
+
+# Tag for advertising, obtainable from @MTProxybot
+# AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
+
+# Uncommenting this do make a proxy harder to detect
+# But it can be incompatible with old clients
+# SECURE_ONLY = True
+# DO_TIME_SYNC = True
